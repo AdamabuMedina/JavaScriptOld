b4db53109344471b6a6483d9bb32f091a8a008b6
AdamabuMedina <adamabujamila@gmail.com>
2020-11-12 18:21:32 +0300

    Завершение работы над дом.заданием

--- 12/js/script.js
+++ 12/js/script.js
@@ -1,3 +1,35 @@
-$('.ball').on('click', function () {
-	alert('Гооол!');
-});
+const percentHeightOfField = 90
+const topPointGoal = 42
+const bottomPointGoal = 50
+const leftEndField = 0
+const rightEndField = 95
+const ballAnimationTime = 1000
+const ball = $(".ball")
+
+ball.click(function () {
+	if (ball.css("left") == leftEndField + "px") {
+		kickTheBall(rightEndField)
+	} else {
+		kickTheBall(leftEndField)
+	}
+})
+
+function kickTheBall(endPoint) {
+	ball.animate({
+		left: endPoint + "%",
+		top: getRandomNumber(percentHeightOfField) + "%"
+	}, ballAnimationTime, function () {
+		sayGoal()
+		ball.toggleClass("rotate360")
+	})
+}
+
+function sayGoal() {
+	if (ball.css("top") >= topPointGoal + "%" && ball.css("top") <= bottomPointGoal + "%") {
+		alert('Гооол!')
+	}
+}
+
+function getRandomNumber(maxRandomNum) {
+	return Math.floor(Math.random() * maxRandomNum);
+}
