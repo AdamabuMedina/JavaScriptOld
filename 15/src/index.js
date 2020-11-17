b5d5b7861cae9e664ec1c92eb332ee30bebaf461
AdamabuMedina <adamabujamila@gmail.com>
2020-11-18 02:56:33 +0300

    модуль 15 разделение на компоненты

--- 15/src/index.js
+++ 15/src/index.js
@@ -12,13 +12,84 @@ class TodoApp extends React.Component {
       super(); // конструктор из React.Component
       // Исходное состояние нашего приложения
       this.state = {
-
+         todos: [
+            { name: "Настроить webpack", checked: true },
+            { name: "Настроить webpack-dev-server", checked: true },
+            { name: "Написать TodoApp", checked: false },
+         ],
+         newTodoText: ""
       }
+   }
+
+   toggleTodo(key) {
+      const todos = this.state.todos.map((todo, i) => {
+         if (key === i) {
+            return {
+               name: todo.name,
+               checked: !todo.checked
+            }
+         } else {
+            return todo
+         }
+      })
+      // Обновляем состояние приложения
+      this.setState({ todos })
+   }
+
+   addTodo(value) {
+      const todos = this.state.todos
+      todos.push({
+         name: this.state.newTodoText,
+         checked: false
+      })
+
+      this.setState({
+         todos,
+         newTodoText: ""
+      })
+   }
+
+   render() {
+      // JSX-синтаксис
+      return (
+         <div>
+            <h2>Todo list</h2>
+            <ol>
+               {
+                  this.state.todos.map((todo, i) => {
+                     const className = todo.checked ? "checked" : ""
+                     return (
+                        <li
+                           className={className}
+                           key={i}
+                           onClick={ev => { this.toggleTodo(i) }}>
+                           {todo.name}
+                        </li>
+                     )
+                  })
+               }
+            </ol>
+            <input
+               type="text"
+               placeholder="Новая задача"
+               value={this.state.newTodoText}
+               onChange={ev => {
+                  this.setState({ newTodoText: ev.target.value })
+               }}
+               onKeyUp={ev => {
+                  if (ev.keyCode === 13) {
+                     this.addTodo()
+                  }
+               }}
+            />
+         </div>
+
+      )
    }
 }
 
 
 ReactDOM.render(
-   <HelloWorld />,
+   <TodoApp />,
    document.querySelector("#app")
 )
--- 15/webpack.config.js
+++ 15/webpack.config.js
@@ -10,8 +10,13 @@ module.exports = {
       rules: [
          {
             test: /\.js$/,
-            use: ["babel-loader"]
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env', "@babel/preset-react"]
+               }
+            }
          },
-      ]
+      ],
    }
 }
