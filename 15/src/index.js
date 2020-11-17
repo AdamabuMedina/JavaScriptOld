1c8e10a246240c72ceaf34b40305e97cd50b8d0a
AdamabuMedina <adamabujamila@gmail.com>
2020-11-18 02:04:20 +0300

    Модуль 15 урок 4

--- 15/src/index.js
+++ 15/src/index.js
@@ -1,9 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function HelloWorld() {
+// React-компонент (функциональный)
+const HelloWorld = () => {
    return <h1>Hello React 123</h1>
 }
+
+// React-компонент (class-based)
+class TodoApp extends React.Component {
+   constructor() {
+      super(); // конструктор из React.Component
+      // Исходное состояние нашего приложения
+      this.state = {
+
+      }
+   }
+}
+
 
 ReactDOM.render(
    <HelloWorld />,
