0c76ac977ad9fe58a69805a83bb8ea61fa736f13
AdamabuMedina <adamabujamila@gmail.com>
2020-11-21 11:06:00 +0300

    Закончил домашнее задание

--- 15-home-work/src/App.js
+++ 15-home-work/src/App.js
@@ -1,67 +1,109 @@
-import React, { useState } from 'react';
-import Modal from './Modal/Modal';
-import CommentsList from './Comments/CommentList';
-import Context from './Context';
+import React, { Component } from "react"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  const [comments, setComments] = useLocalStorage("Comments", []);
+class App extends Component {
 
-  const removeComment = (id) => {
-    setComments(comments.filter(comment => comment.id !== id));
-  };
-
-  const addComment = (event) => {
-    event.preventDefault(event);
-    setComments(comments.concat([
-      {
-        id: Date.now(),
-        author: event.target.name.value,
-        text: event.target.comment.value,
-        date: new Date().toLocaleString()
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+      form: {
+        name: "",
+        comment: "",
       }
-    ]))
+    }
   }
 
-  return (
-    <Context.Provider value={{ removeComment }}>
-      <div>
-        <div className="sidenav">
-          <Modal
-            onSubmit={addComment}
-          />
+  componentDidMount() {
+    if (localStorage.getItem("state")) {
+      this.setState({ ...JSON.parse(localStorage.getItem("state")) })
+    }
+  }
+
+  addComment = () => {
+    this.state.form.name = this.state.form.name.replace(/<[^>]+>/g, "")
+    this.state.form.comment = this.state.form.comment.replace(/<[^>]+>/g, "")
+    if (this.state.form.name.length === 0) {
+      alert("Введите имя")
+    }
+    else if (this.state.form.comment.length === 0) {
+      alert("Введите комментарий")
+    }
+    else {
+      this.setState({
+        comments: [
+          ...this.state.comments,
+          {
+            id: this.state.comments.length ? this.state.comments.reduce((p, c) => p.id > c.id ? p : c).id + 1 : 1,
+            name: this.state.form.name,
+            comment: this.state.form.comment,
+            date: new Date().toLocaleString()
+          }
+        ],
+        form: {
+          name: "",
+          comment: ""
+        }
+      }, () => localStorage.setItem("state", JSON.stringify(this.state)))
+    }
+  }
+
+
+  removeComment = (id) => {
+    this.setState({
+      comments: this.state.comments.filter(comment => comment.id !== id)
+    }, () => localStorage.setItem("state", JSON.stringify(this.state)))
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.name)
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="form-group">
+        <div className="form-group">
+          <label>Имя: <input
+            class="form-control"
+            id="exampleInputEmail1"
+            type="text"
+            value={this.state.form.name}
+            name="name"
+            onChange={this.handleChange} /></label>          <label>Коментарий:<textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              name="comment"
+              value={this.state.form.comment}
+              onChange={this.handleChange}></textarea>
+          </label>          <button className="btn btn-primary btn-lg" onClick={this.addComment}
+          >Добавить комментарий</button>
         </div>
-        <div className="main">
-          {comments.length
-            ? (<CommentsList comments={comments} />)
-            : (<p>No comments yet!</p>)}
-        </div>
+        {this.state.comments.map(comment =>
+          <div className="card">
+            <div className="card-header">
+              <h5 className="card-title">{comment.name}</h5>
+            </div>
+            <div className="card-body" key={comment.id}>
+              <h6 className="card-subtitle mb-2 text-muted">
+                <strong> {comment.id}.</strong> {comment.date}: </h6>
+              <p className="card-text">{comment.comment}</p>
+              <button
+                className="btn btn-primary"
+                onClick={this.removeComment.bind(null, comment.id)}>
+                Удалить
+              </button>
+            </div>
+          </div>)}
       </div>
-    </Context.Provider>
-  );
+    )
+  }
 }
 
-function useLocalStorage(key, initialValue) {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
-    }
-  });
-  const setValue = value => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return [storedValue, setValue];
-}
-
-export default App;
+export default App
